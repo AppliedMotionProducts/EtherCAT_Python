ca5bd6c9edb79dcc36c685f8f8bae129a35e18e4
Applied Motion Products <marketing@applied-motion.com>
2025-04-15 15:43:57 -0700

    Create Q_Mode.py

---
+++ Q_Mode.py
@@ -0,0 +1,110 @@
+import pysoem
+import ctypes
+import time
+import threading
+
+pd_thread_stop_event = threading.Event()
+master = pysoem.Master()
+master.open("\\Device\\NPF_{ABF1453B-31C2-47F8-B7DC-10C3C0C7D7A5}")
+actual_wkc = 0
+
+#TxPDO
+class InputPdo(ctypes.Structure):
+    _pack_ = 1
+    _fields_ = [
+        ('Errorword', ctypes.c_uint16),
+        ('statusword', ctypes.c_uint16),
+        ('position_actual_value', ctypes.c_int32),
+        ('velocity_actual_value', ctypes.c_int32),
+        ('followerrorcodevalue', ctypes.c_uint32)
+    ]
+
+#RxPDO
+class OutputPdo(ctypes.Structure):
+    _pack_ = 1
+    _fields_ = [
+        ('controlword', ctypes.c_uint16),
+        ('modes_of_operation', ctypes.c_int8),
+        ('target_position', ctypes.c_int32),
+        ('target_velocity', ctypes.c_int32),
+    ]
+
+
+modes_of_operation = {
+    'No mode': 0,
+    'Profile position mode': 1,
+    'Profile velocity mode': 3,
+    'Profile_torque_mode': 4,
+    'Homing mode': 6,
+    'Cyclic synchronous position mode': 8,
+    'Cyclic synchronous velocity mode': 9,
+    'Cyclic synchronous torque mode': 10,
+    'Q_mode': -1, # manufacturer specific mode
+}
+
+
+def config_func():
+    global device
+    # Q Segment Number
+    device.sdo_write(0x2007, 0, bytes(ctypes.c_int8(1))) # execute Q segment 1
+
+
+def processdata_thread():
+    while not pd_thread_stop_event.is_set():
+        master.send_processdata()
+        actual_wkc = master.receive_processdata(10000)
+        if not actual_wkc == master.expected_wkc:
+            print('incorrect wkc')
+        time.sleep(0.01)
+
+
+if master.config_init() > 0:
+    device = master.slaves[0]
+    device.config_func = config_func()
+    master.config_map()
+
+    if master.state_check(pysoem.SAFEOP_STATE, 50_000) == pysoem.SAFEOP_STATE:
+        master.state = pysoem.OP_STATE
+
+        proc_thread = threading.Thread(target=processdata_thread)
+        proc_thread.start()
+
+        master.write_state()
+        master.state_check(pysoem.OP_STATE, 5_000_000)
+
+        if master.state == pysoem.OP_STATE:
+            output_data = OutputPdo()
+            output_data.modes_of_operation = modes_of_operation['Q_mode']
+
+            for control_cmd in [6, 7, 15, 31]:
+                output_data.controlword = control_cmd
+                device.output = bytes(output_data)  # that is the actual change of the PDO output data
+                master.send_processdata()
+                master.receive_processdata(1_000)
+                time.sleep(0.01)  # you may need to increase this sleep
+
+            try:
+                while 1:
+                    master.send_processdata()
+                    master.receive_processdata(1_000)
+                    time.sleep(0.01)
+            except KeyboardInterrupt:
+                print('stopped')
+            # zero everything
+            device.output = bytes(len(device.output))
+            master.send_processdata()
+            master.receive_processdata(1_000)
+            pd_thread_stop_event.set()
+            proc_thread.join()
+
+        else:
+            print('failed to got to OP_STATE')
+
+    else:
+        print('failed to got to safeop state')
+    master.state = pysoem.PREOP_STATE
+    master.write_state()
+else:
+    print('no device found')
+
+master.close()
